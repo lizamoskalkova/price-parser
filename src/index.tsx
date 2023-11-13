3df0ac8dcc410330f52d8caf2294359029743400
Elizaveta Moskalkova <esmoskalkova@sberbank.ru>
2023-11-13 14:01:32 +0300

    fix build

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./api/store.ts";
-import { App } from "./App.tsx";
+import { App } from "./App";
+import { store } from "./api/store";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
